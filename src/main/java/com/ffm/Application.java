423e16e689667dac6ebc9ac309c19311a64d10b8
tburtondurham <tburtondurham@gmail.com>
2020-09-18 18:10:51 +0100

    db

--- src/main/java/com/ffm/Application.java
+++ src/main/java/com/ffm/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
-import java.util.Arrays;
+import java.util.Arraymvnw spring-boot:runs;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
