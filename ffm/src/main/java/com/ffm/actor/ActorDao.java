ba6a4ac449ad2cd9dc272259c75ebd32e49b89e2
tburtondurham <tburtondurham@gmail.com>
2020-09-28 16:16:36 +0100

    Created getActorsForFilm - might not be correct

--- ffm/src/main/java/com/ffm/actor/ActorDao.java
+++ ffm/src/main/java/com/ffm/actor/ActorDao.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class ActorDao {
@@ -13,25 +14,25 @@ public class ActorDao {
   JdbcTemplate jdbcTemplate;
 
   public List<String> getAllActors() {
-
-    List<String> name = jdbcTemplate.queryForList("select * from actor", String.class);
-    return name;
+    return jdbcTemplate.queryForList("select * from actor", String.class);
 
   }
 
   public Actor getActor(Integer actorId) {
-
     return jdbcTemplate.queryForObject("select * from actor where id="+actorId, new ActorRowMapper());
+  }
 
+  // IS THIS CORRECT!??
+  // I want to return an array of actor objects for the film object to sort through...
+
+  public List<Map<String, Object>> getActorsForFilm(Integer filmId) {
+    return jdbcTemplate.queryForList(String.format("SELECT a. *, (SELECT SUM(r.RATING_VALUE)  FROM ACTOR_RATING r WHERE r.GENRE_ID=f.GENRE_ID AND a.id=r.ACTOR_ID)  as rating FROM film_actor fa LEFT JOIN actor a on fa.ACTOR_ID=a.id LEFT JOIN film f on f.id=fa.FILM_ID WHERE fa.FILM_ID = %s AND a.DELETED=0;", filmId), new ActorRowMapper());
   }
   public Integer getActorRatingByGenre(Integer genreId, Integer actorId) {
 
-    Integer integer = jdbcTemplate
-        .queryForObject(String.format("SELECT SUM(r.RATING_VALUE) as total_value FROM ACTOR_RATING r LEFT JOIN ACTOR a on a.id=r.actor_id AND r.GENRE_ID=%s WHERE a.id = %s;", genreId, actorId), Integer.class);
-    return integer;
+    return jdbcTemplate.queryForObject(String.format("SELECT SUM(r.RATING_VALUE) as total_value FROM ACTOR_RATING r LEFT JOIN ACTOR a on a.id=r.ACTOR_ID AND r.GENRE_ID=%s WHERE a.id = %s;", genreId, actorId), Integer.class);
 
   }
-
   public void saveActor(Actor actor) {
     jdbcTemplate.execute("INSERT INTO ACTOR values.......");
 
