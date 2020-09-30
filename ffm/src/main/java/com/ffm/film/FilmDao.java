79aeec0f2ce326e5fd73070e673debcb7922d587
tburtondurham <tburtondurham@gmail.com>
2020-09-30 11:35:15 +0100

    FilmDao Correction

--- ffm/src/main/java/com/ffm/film/FilmDao.java
+++ ffm/src/main/java/com/ffm/film/FilmDao.java
@@ -1,7 +1,5 @@
 package com.ffm.film;
 
-import com.ffm.actor.Actor;
-import com.ffm.actor.ActorRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
